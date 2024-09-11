a09f7e69f9a8178c93e0ceaf20d3d4f1a84034d3
Prabhat Raj Poudel <poudelprabhas88@gmail.com>
2024-09-11 21:16:36 +0545

    changess

--- src/app/components/ImageComponent.tsx
+++ src/app/components/ImageComponent.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ImageComponent = () => {
   return (
     <View>
-      <Text> ImageComponent</Text>
+      <Text> Image Component</Text>
    <Image source={require('@src/assets/me.jpg')}style={{width:200, height:200}} /> 
     </View>
   )
